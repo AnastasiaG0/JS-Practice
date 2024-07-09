4549075d5332f0349ee8eab2d9b2661b4230e6c3
Anastasia <gummel2005@gmail.com>
2024-07-09 13:15:31 +0300

    add-6_7_1

---
+++ Practice6_7_1.js
@@ -0,0 +1,34 @@
+/*Создайте функцию с названием getOlderUser(), которая будет определять, кто из двух пользователей старше.
+Аргументами функции являются два пользователя в виде двух объектов.
+Функция должна вернуть с помощью команды return имя старшего пользователя.
+Созданную функцию нужно вызвать, передав ей два объекта: user1 и user2.
+Результат, который вернёт функция, необходимо вывести в консоль.*/
+
+function getOlderUser(user1, user2) {
+    if (user1.age > user2.age) {
+        return user1.name
+    }
+    else if (user1.age == user2.age) {
+        let arr = []
+        arr.push(user1.name)
+        arr.push(user2.name)
+        return arr.join(", ")
+    }
+    else {
+        return user2.name
+    }
+}
+
+let user1 = {
+    name: 'Игорь',
+    age: 17
+}
+
+let user2 = {
+    name: 'Оля',
+    age: 21
+}
+
+console.log(getOlderUser(user1, user2))
+
+//node Practice6_7_1.js
